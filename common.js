2dea0775f4e9b6c29b1b246c1983dd7d4babdb6c
rechar86 <40575870+rechar86@users.noreply.github.com>
2018-08-18 16:32:24 +0800

    DEV1.0.3

--- common.js
+++ common.js
@@ -1,13 +1,12 @@
 function changeUrl(urlAddr) {
-    var root = window.location.protocol + '//' + window.location.host;
+    var root = window.location.protocol+'//'+window.location.host+'/';
     alert('path1='+ root);
     var pathanme = window.location.pathname;
     var pathArray = pathanme.split("/");
     var path = pathArray[pathArray.length-2];	
-    alert('path='+ path);
-    root = root + '/' + path;
-    root = root + '/' + urlAddr + '?ts=' + Date.now();
-    alert('path2='+ root);
+    alert('path2='+ path);
+    root = root + path + '/' + urlAddr + '?ts=' + Date.now();
+    alert('path3='+ root);
     window.location.href = root;
 }
 
